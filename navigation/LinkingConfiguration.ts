41ed38aa39f78e4dfe44646596056175859350ea
Ivaneev Ivan <ivaneev.ivane@gmail.com>
2022-08-24 21:08:59 +0300

    Add bottomTab buttons

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -1,36 +1,35 @@
-/**
- * Learn more about deep linking with React Navigation
- * https://reactnavigation.org/docs/deep-linking
- * https://reactnavigation.org/docs/configuring-links
- */
+import { LinkingOptions } from "@react-navigation/native";
+import * as Linking from "expo-linking";
 
-import { LinkingOptions } from '@react-navigation/native';
-import * as Linking from 'expo-linking';
-
-import { RootStackParamList } from '../types';
+import { RootStackParamList } from "../types";
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.createURL('/')],
-  config: {
-    screens: {
-      Root: {
-        screens: {
-          TabOne: {
-            screens: {
-              TabOneScreen: 'one',
-            },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
-            },
-          },
-        },
-      },
-      Modal: 'modal',
-      NotFound: '*',
-    },
-  },
+	prefixes: [Linking.createURL("/")],
+	config: {
+		screens: {
+			Root: {
+				screens: {
+					Home: {
+						screens: {
+							TabOneScreen: "one",
+						},
+					},
+					Search: {
+						screens: {
+							TabTwoScreen: "two",
+						},
+					},
+					Account: {
+						screens: {
+							TabOneScreen: "three",
+						},
+					},
+				},
+			},
+			Modal: "modal",
+			NotFound: "*",
+		},
+	},
 };
 
 export default linking;
